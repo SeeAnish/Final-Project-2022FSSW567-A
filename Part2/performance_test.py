f9995ca8ca21430b51c7761869b51b920b955a7f
SeeAnish <amalhot6@stevens.edu>
2022-12-14 20:48:05 -0500

    perf

--- Part2/performance_test.py
+++ Part2/performance_test.py
@@ -1,70 +1,61 @@
 import json
 import unittest
 import MRTD
-import pandas as pd
-from matplotlib import pyplot as plt
 import time
 import csv
 from MRTDtest import main as run_unit_test
 
-def speed_test_verify_endoced(num , fileName):
+def speed_test_verify_endoced_with_test( first , last , fileName , step_num):
     with open('records_encoded.json') as json_file:
-        #tic = time.perf_counter()
+        tic = time.perf_counter()
         data = json.load(json_file)
         records = data["records_encoded"]
         with open(fileName, 'w', newline='') as time_data_file:
             time_writer = csv.writer(time_data_file)
-            time_writer.writerow(["SN", "Execution_time_without_test"])
+            time_writer.writerow(["Lines Executed", "time in seconds with test"])
             tic = time.perf_counter()
-            for i in range(0, num):
+            run_unit_test()
+            for i in range(first, last , step_num):
                 MRTD.vertify(records[i])
                 # print("Record Number #", i + 1)
                 # print(records[i], "\n", "\n")
                 t2 = time.perf_counter()
-                time_writer.writerow([i + 1, f"{t2 - tic:0.8f}"])
+                if i == 99:
+                    time_writer.writerow([i + 1, f"{t2 - tic:0.8f}"])
+                elif (i+1) % 1000 == 0:
+                    time_writer.writerow([i + 1, f"{t2 - tic:0.8f}"])
             print(f"csv file updated - {fileName}.")
-        tac = time.perf_counter()        
-        print(f"Decoded {num} files in - ", f"{tac - tic:0.4f}", "ms")
+        tac = time.perf_counter()
 
-def speed_test_verify_endoced_with_unittest(num , fileName):
+        print(f"Verified {last-first} files in - ", f"{tac - tic:0.4f}", "ms")
+
+
+def speed_test_verify_endoced_without_test( first , last , fileName , step_num):
     with open('records_encoded.json') as json_file:
+        tic = time.perf_counter()
         data = json.load(json_file)
         records = data["records_encoded"]
         with open(fileName, 'w', newline='') as time_data_file:
             time_writer = csv.writer(time_data_file)
-            time_writer.writerow(["SN", "Execution_time_with_test"])
+            time_writer.writerow(["Lines Executed", "time in seconds without test"])
             tic = time.perf_counter()
-            run_unit_test()
-            for i in range(0, num):
+            for i in range(first, last , step_num):
                 MRTD.vertify(records[i])
                 # print("Record Number #", i + 1)
                 # print(records[i], "\n", "\n")
-                tok = time.perf_counter()
-                time_writer.writerow([i + 1, f"{tok - tic:0.8f}"])
+                t2 = time.perf_counter()
+                if i == 99:
+                    time_writer.writerow([i + 1, f"{t2 - tic:0.8f}"])
+                elif (i+1) % 1000 == 0:
+                    time_writer.writerow([i + 1, f"{t2 - tic:0.8f}"])
             print(f"csv file updated - {fileName}.")
-        tac = time.perf_counter()        
-        print(f"Decoded {num} files in - ", f"{tac - tic:0.4f}", "ms")
-    
-def plotting_graph1():
-    plt.rcParams["figure.figsize"] = [7.00, 3.50]
-    plt.rcParams["figure.autolayout"] = True
-    columns = ["SN", "Execution_time_without_test"]
-    df = pd.read_csv("encode_verify_100_entries.csv", usecols=columns)
-    print("Contents in csv file:", df)
-    plt.plot(df.SN, df.Execution_time_without_test)
-    plt.show()
+        tac = time.perf_counter()
+
+        print(f"Verified {last-first} files in - ", f"{tac - tic:0.4f}", "ms")
 
 
-def main():
-    k = [100, 1000]
-    speed_test_verify_endoced(100 , 'encode_verify_100_entries.csv')
-    plotting_graph1()
-    speed_test_verify_endoced(1000 , 'encode_verify_100_entries.csv')
-    plotting_graph1()
-    
-    #speed_test_verify_endoced_with_unittest(i, 'unittest_encode_entries.csv')
 
 if __name__ == '__main__':
-    main()
-    
-    #new data
+
+    speed_test_verify_endoced_with_test(0 , 10000 , "encode with test case.csv" , 1)
+    speed_test_verify_endoced_without_test(0, 10000, "encode without test case.csv", 1)
