f178387403a3a7aff3c075ef20f77860af485f68
Prakhar564 <pagarwa8@stevens.edu>
2022-12-14 22:56:50 -0500

    Update MRTD.py

--- Part2/MRTD.py
+++ Part2/MRTD.py
@@ -1,18 +1,20 @@
 from string import ascii_uppercase, digits
 from tkinter import END
+#Checking digit to document number field
 def algorithm(string: str) -> str:
     printable = digits + ascii_uppercase
     string = string.upper().replace("<", "0")
     weight = [7, 3, 1]
     summation = 0
     for i in range(len(string)):
-        c = string[i]
-        if c not in printable:
-            raise ValueError("%s contains invalid characters" % string, c)
-        summation += printable.index(c) * weight[i % 3]
+        Validation = string[i]
+        if Validation not in printable:
+            raise ValueError("%s contains invalid characters" % string, Validation)
+        summation += printable.index(Validation) * weight[i % 3]
     summation %=10
     return summation
 
+#It is seperating 2 lines of MRTD and sending it to algorithm  
 def vertify (string0: str) -> str:
     string1 = string0[45:]
     passport = string1[0:9]
